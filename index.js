cd312a68623c4da033227ecc3e0dce00aef7bd90
Kyle Mitchell <kyle@kemitchell.com>
2015-08-31 19:17:11 -0700

    Fix semicolon error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var has = function(key) {
   return function(argument) {
     return argument.hasOwnProperty(key) } }
 
-['blank', 'definition', 'reference', 'use']
+;['blank', 'definition', 'reference', 'use']
   .forEach(function(type) {
     exports[type] = has(type) })
 
